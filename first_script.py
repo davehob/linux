aa8ca05b21be040deb3eba13765c38b47771cd13
David H <hoblea@gmail.com>
2023-02-21 00:59:18 -0800

    Update first_script.py

--- first_script.py
+++ first_script.py
@@ -1,5 +1,6 @@
 import os
 
-cmd = 'sudo apt update && sudo apt upgrade && sudo apt install vim git -y'
-
+cmd = 'sudo apt update && sudo apt upgrade && sudo apt install vim git qemu-guest-agent -y'
+cmd2 = 'systemctl enable qemu-guest-agent && systemctl start qemu-guest-agent && systemctl status qemu-guest-agent'
 os.system(cmd)
+os.system(cmd2)
